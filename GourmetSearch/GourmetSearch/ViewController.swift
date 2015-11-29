78ad1c908b877ba7932414f2c9f5d429959ab55c
Jumbo <kurekenpa@gmail.com>
2015-11-29 11:25:49 +0900

    init

---
+++ GourmetSearch/GourmetSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GourmetSearch
+//
+//  Created by JW on 11/29/27 H.
+//  Copyright © 27 Heisei woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
