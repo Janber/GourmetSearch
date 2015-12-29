cc3bb015e2fa2c7f37bf2851b46b24974afbd89e
Jumbo <kurekenpa@gmail.com>
2015-12-29 20:21:04 +0900

    ver.0.1 finish

---
+++ GourmetSearch/GourmetSearch/AboutViewController.swift
@@ -0,0 +1,52 @@
+//
+//  AboutViewController.swift
+//  GourmetSearch
+//
+//  Created by JW on 12/14/27 H.
+//  Copyright © 27 Heisei woo. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   // MARK: - UITableViewDelegate
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 110
+        }
+        return 44
+    }
+    
+    
+    // MARK: - UITableViewDataSource
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+//        if indexPath.row == 0 {
+//            let cell = tableView.dequeueReusableCellWithIdentifier("AboutApp")! as UITableViewCell
+//            return cell
+//        }
+//        
+        let cell = tableView.dequeueReusableCellWithIdentifier("License")! as UITableViewCell
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+}
---
+++ GourmetSearch/GourmetSearch/LicenseDetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LicenseDetailViewController.swift
+//  GourmetSearch
+//
+//  Created by JW on 12/29/27 H.
+//  Copyright © 27 Heisei woo. All rights reserved.
+//
+
+import UIKit
+
+class LicenseDetailViewController: UIViewController {
+
+    @IBOutlet weak var text: UILabel!
+    @IBOutlet weak var textHeight: NSLayoutConstraint!
+    
+    var name = ""
+    var license = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        text.text = license
+        title = name
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    override func viewDidLayoutSubviews() {
+        let frame = CGSizeMake(text.frame.size.width, CGFloat.max)
+        textHeight.constant = text.sizeThatFits(frame).height
+        view.layoutIfNeeded()
+    }
+
+}
